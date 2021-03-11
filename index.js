330124d69f5b23f2a58bb6e52d10ffd0e234ae3a
Hunter Fleming <63882306+weirdbandkid@users.noreply.github.com>
2021-03-11 14:29:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const RPC = require('discord-rpc');
+const rpc = new RPC.Client({
+    transport: "ipc"
+});
+
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: "Line 1",
+        state: "Line 2",
+        startTimestamp: new Date(),
+        largeImageKey: "discord", //must match images in the application
+        largeImageText: "Text 1",
+        smallImageKey:"github", //must match images in the application
+        smallImageText: "Text 2",
+        
+    });
+    console.log("Rich Presence is now active");
+})
+
+rpc.login({
+    clientId: "Enter your client ID here" //https://discord.com/developers/applications
+})
