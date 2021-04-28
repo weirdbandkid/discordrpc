a57c50fd74d44ab5a97f3ccef9ae6fa744205cb3
weirdbandkid_ <chicagofirefan2005@outlook.com>
2021-04-28 17:15:43 -0400

    Added Logo to SH and BAT

--- DiscordRPC.sh
+++ DiscordRPC.sh
@@ -1,3 +1,28 @@
 # This is for Linux only!!
+echo              `::::.        /y/                                                          :`              -:::::-    .::::::`      `::::`              
+echo              .+mdoNo       :s:                                                         -M:              hm++++dd   oMo+++yN.    /Ns+++.              
+echo                dh -No    `sss/      `+sssss:     :sssss`   `/ssso:    +/.+sso.    .osso+M:              hm----hm   oM`   :M-   /N/                   
+echo                dh  /M-    ..my      +Ms:..:+   .dh/....    mh-../No   mNmo..hd   :No..+dM:              hNomNs+.   oM/:::sM.  .Mo                    
+echo                dh  ym`      my       -oydy+`   +M`        `Mo    my   md`   `.   +M.   :M:              hd .dd`    oMo++++.   `dd`                   
+echo                dh`ym.       my      .-  `-mh   -Ns.       `Ny`  .Ns   mh         /M:  -yM:              hd  `ym.   oM`         `dd`                  
+echo              .hddhy.     `yhddhs    :yhhhhy-    `+hhhhh`   -shhhy+`   so          /yhhyod-              os    oy   /h           `shhhh-              
+echo               ````        `````       ````         ````      ```                    ``                                            ````               
+echo                                                                 ./                                                                                   
+echo                                                                 /M`                                                                                  
+echo                                                                 /M+yyys-    y/    o+                                                                 
+echo                                                                 /Mh:..+N-   dh`  .No                                                                 
+echo                                                                 /M.   .M/   .ms `dh`                                                                 
+echo                                                                 /Mh:..oN-    -Nshd`                                                                  
+echo                                                                 -y/syys-      -mm.                                                                   
+echo                                                                             :/sN-                                                                    
+echo                                                                             /++-                                                                     
+echo                                  ``                                                                                         `                        
+echo                                  dm-                       -o   .o                                      .o`  `o.           +ms            `o`        
+echo                                 `:/`                       oM`  /M`                                     /M.  .M+           ./-            :M:        
+echo        .d-    +y   `ohhhhs.    :hhd-     `d/+hhhy.   `ohhhyhM`  /Mohhhy:     -hhhhy`   ss+yhhy-   `+hhhyyM.  .M+  :yo     yhds       /hhhysM:        
+echo        `M/ +: hm   hm.  `ym      -M:     .MNs.  do   yN.  :mM`  /My.  /M:    .---:Mo   dm/.  dh   oM.  -dM.  .Mo+dh:        dh      +M-  -hM:        
+echo        `Mo Ny dh   dNhhhhdd      -M:     .M+         hm    oM`  /M.   .M/   hdooosMs   dd    hd   sN    +M.  .MNdm/         dh      oM`   :M:        
+echo         ymhmNsM/   sN/.....    `./M+.`   .M+         oN/.-sNM`  /Md/.-sN-   Ny..-+Ny   dd    yN   +N+.-omM.  .M+ -ym+`    ..dd..    /M+..+mM:        
+echo         `so`-so     -osssso    -sssss:   `o-          -oss+:o   .s-oss+`    .osss++/   //    :o    -oss+:s`  `o-   .o:    ssssso     .oss+-s`
 cd /home/user/location # must change the location to work
 node .
