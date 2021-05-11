1204480dde2f5edfa3f81de5cc31d85caa346851
weirdbandkid_ <Chicagofirefan05@outlook.com>
2021-05-11 09:25:04 -0400

    Added comments to show what everything does

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const RPC = require('discord-rpc');
+const RPC = require('discord-rpc');// how this works lol
 const rpc = new RPC.Client({
-    transport: "ipc"
+    transport: "ipc" // don't know what this does, I just read documentation
 });
 
 rpc.on("ready", () => {
     rpc.setActivity({
-        details: "Line 1",
-        state: "Line 2",
-        startTimestamp: new Date(),
+        details: "Line 1", // text under Application name
+        state: "Line 2", // text under details 
+        startTimestamp: new Date(), // If you want to show how long the status has been running, if you don't want it, put // infront of the line or just delete this line
         largeImageKey: "discord", //must match images in the application
-        largeImageText: "Text 1",
+        largeImageText: "Text 1", // This is the text above the large image
         smallImageKey:"github", //must match images in the application
-        smallImageText: "Text 2",
+        smallImageText: "Text 2", // this is the text above the small image
                 buttons: [
-            { label: "Text1", url: "URL1" },
-            { label: "Text2", url: "URL2" }
+            { label: "Text1", url: "URL1" }, //ex. { label: "My Dev Discord Server", url: "https://discord.gg/46HQ9rJ" },
+            { label: "Text2", url: "URL2" } // ex. { label: "My Main Discord", url: "https://discord.gg/cEhU6VF" },
         ]
     });
-    console.log("Rich Presence is now active");
+    console.log("Rich Presence is now active"); // lets you know it worked
 })
 
 rpc.login({
